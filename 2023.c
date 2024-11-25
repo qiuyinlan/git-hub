8d613c25f93efafe465c06760002722d96478b92
xuzichun <2978500991@qq.com>
2024-11-25 14:05:28 +0800

    11/25

--- 2023.c
+++ 2023.c
@@ -1,22 +1,31 @@
 #include<stdio.h>
-struct Student {
-char *name;
-int age;
-};
-void initializeStudent( struct Student *student, const char *name,
-int age) {
-student->name = (char *)malloc(strlen(name) + 1 );
-strcpy(student->name, name);
-student->age = age;
-}
-int main(void) {
-struct Student s1, s2;
-initializeStudent(&s1, "Tom", 18 );
-initializeStudent(&s2, "Jerry", 28 );
-s1 = s2;
-printf("s1的姓名: %s 年龄: %d\n", s1.name, s1.age);
-printf("s2的姓名: %s 年龄: %d\n", s2.name, s2.age);
-free(s1.name);
-free(s2.name);
-return 0 ;
+int main()
+{
+    
+    int count=0;
+    for(int i=100;i<=200;i++)
+    {
+        int k=0;
+        for(int j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                k=1;
+                break;
+            }
+        }
+         if(k==0)
+         {
+              printf("%-5d",i);
+                count++;
+        }
+            
+        if(count%8==0&&count!=0)
+        {   
+             printf("\n");
+             
+        }
+       
+        
+    }
 }
