95f0eff8a82173e81f845bba2373fe5de3d8e5b6
xuzichun <2978500991@qq.com>
2024-11-28 21:11:14 +0800

    11/28

--- 2023.c
+++ 2023.c
@@ -1,31 +1,5 @@
 #include<stdio.h>
 int main()
 {
-    
-    int count=0;
-    for(int i=100;i<=200;i++)
-    {
-        int k=0;
-        for(int j=2;j<i;j++)
-        {
-            if(i%j==0)
-            {
-                k=1;
-                break;
-            }
-        }
-         if(k==0)
-         {
-              printf("%-5d",i);
-                count++;
-        }
-            
-        if(count%8==0&&count!=0)
-        {   
-             printf("\n");
-             
-        }
-       
-        
-    }
+    printf("Hello World\n");
 }
