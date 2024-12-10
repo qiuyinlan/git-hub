cc5b3eb40e9a71d5e40cf1933a3833b2907716b4
xuzichun <2978500991@qq.com>
2024-12-10 19:38:53 +0800

    12/10

--- .c
+++ .c
@@ -1,5 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int Count_Digit ( const int N, const int D );
+
 int main()
 {
-    printf("hello world\nhello");
+    int N, D;
+    
+    scanf("%d %d", &N, &D);
+    printf("%d\n", Count_Digit(N, D));
+    return 0;
 }
+int Count_Digit ( const int N, const int D ){
+    int count=0,i,n;
+    n=N;
+    for(i=n;n%10!=0;){
+        if(D==n%10){
+            count++;
+        }
+        n=n/10;
+    }
+    return count;
+}
+/* 你的代码将被嵌在这里 */
